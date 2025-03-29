4f23cb5ec00c2d41cde95d63cd68c24478f0e115
hng19 <hoangvanhong142@gmail.com>
2025-03-29 15:51:58 +0700

    first

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using System.Windows.Forms;
+using Autodesk.Revit.UI.Selection;
+
+namespace A003_SelectEdge
+{
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit
+    .Attributes.TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData
+        commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+            IList<Reference> referenceCollection = uidoc.Selection.PickObjects(ObjectType.Edge);
+
+            MessageBox.Show("You have selected total" + referenceCollection.Count.ToString()+ "Edges.");
+            return Result.Succeeded;
+        }
+    }
+}
